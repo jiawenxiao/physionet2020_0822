44a2d7f6efc1f3ec1f7cd9a3edb430a9a539feb4
Wenxiao Jia <1950461210@qq.com>
2020-08-24 04:20:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ class Config:
     #训练的模型名称
     model_name = 'resnet34'
     #在第几个epoch进行到下一个state,调整lr
-    stage_epoch = [10,13]
+    stage_epoch = [10,12]
     #训练时的batch大小
     batch_size = 64
     #label的类别数
     num_classes = 24
     #最大训练多少个epoch
-    max_epoch = 16
+    max_epoch = 14
     #目标的采样长度
     target_point_num = 5000
     #保存模型的文件夹
